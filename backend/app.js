041159bb2c053e1a520561de0d42dd7fb17abe26
Mahdi <mahdiopera110@gmail.com>
2024-11-13 11:27:55 +0330

    update app.js
    add cors and add update routes and add cookie parser.

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,23 @@ import morgan from 'morgan'
 import dotenv from 'dotenv'
 import HandleError from './utils/handleError.js'
 import catchError from './utils/catchError.js'
+import authRouter from './Routes/auth.js'
+import nobitexRouter from './Routes/nobitex.js'
+import cookieParser from 'cookie-parser'
+import authMiddleware from './Middleware/authMiddleware.js'
 
 const app = express()
 dotenv.config({path:"./.env"})
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true // برای ارسال کوکی‌ها ضروری است
+  }));
+  app.use(cookieParser());
 
-// app.use('/api/v1/auth')
-// app.use('/api/v1/dashboard')
+app.use('/api/v1/auth',authRouter)
+app.use('/api/v1/nobitex',authMiddleware,nobitexRouter)
 app.use('*',(req,res,next)=>{
     next(new HandleError("مسیر پیدا نشد !",404))
 })
