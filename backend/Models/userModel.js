4deaa6acfa51e5108b469458a066a6c3706bc57f
Mahdi <mahdiopera110@gmail.com>
2024-11-13 11:30:35 +0330

    update user model

--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -11,6 +11,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    username: {
+      type: String,
+      unique: true,
+      required: [true, "Username is required."],
+    },
     email: {
       type: String,
       required: true,
@@ -28,7 +33,7 @@ const userSchema = new mongoose.Schema(
     },
     tokens: {
       type: Map,
-      of:String
+      of: String,
     },
   },
   { timestamps: true }
